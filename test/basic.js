8cd3a45ea6f9dc2c83ca054385cba85a7494161a
Gabe <gabe@bitpay.com>
2014-09-02 13:53:18 -0400

    convert test to 2-space tabs

--- test/basic.js
+++ test/basic.js
@@ -2,72 +2,72 @@ var assert = require("assert")
 var dolarblue = require('../lib/dolar-blue');
 
 describe('exchange rates', function(){
-    this.timeout(5000);
+  this.timeout(5000);
   
   
-    describe('getData', function(){
+  describe('getData', function(){
 
-        
-        it('should return current dolar blue BUY and SELL rate, and a datetime', function(done){
-            dolarblue.getData(function(err, data) {
-                assert.ok(!err);
-                assert.ok( data.buy > 0);
-                assert.ok( data.sell > 0);
+    
+    it('should return current dolar blue BUY and SELL rate, and a datetime', function(done){
+      dolarblue.getData(function(err, data) {
+        assert.ok(!err);
+        assert.ok( data.buy > 0);
+        assert.ok( data.sell > 0);
 
 
-                var d = data.datetime;
-                assert.ok( d > new Date(0) );
-                var before =  new Date('2013-12-16 18:00:00');
-                assert.ok( d > before);
-                done();
-            });
-        });
+        var d = data.datetime;
+        assert.ok( d > new Date(0) );
+        var before =  new Date('2013-12-16 18:00:00');
+        assert.ok( d > before);
+        done();
+      });
     });
+  });
 
-    describe('getData({src:LaNacion})', function(){
-        it('should return current dolar blue BUY and SELL rate, and a datetime', function(done){
-            dolarblue.getData({src:'LaNacion'}, function(err, data) {
-                assert.ok(!err);
-                assert.ok( data.buy > 0);
-                assert.ok( data.sell > 0);
+  describe('getData({src:LaNacion})', function(){
+    it('should return current dolar blue BUY and SELL rate, and a datetime', function(done){
+      dolarblue.getData({src:'LaNacion'}, function(err, data) {
+        assert.ok(!err);
+        assert.ok( data.buy > 0);
+        assert.ok( data.sell > 0);
 
 
-                var d = data.datetime;
-                assert.ok( d > new Date(0) );
-                var before =  new Date('2013-12-16 18:00:00');
-                assert.ok( d > before);
-                done();
-            });
-        });
+        var d = data.datetime;
+        assert.ok( d > new Date(0) );
+        var before =  new Date('2013-12-16 18:00:00');
+        assert.ok( d > before);
+        done();
+      });
     });
+  });
 
-    describe('getData({src:Bluelytics})', function(){
-        it('should return current dolar blue BUY and SELL rate, and a datetime', function(done){
-            dolarblue.getData({src:'Bluelytics'}, function(err, data) {
-                assert.ok(!err);
-                assert.ok( data.buy > 0);
-                assert.ok( data.sell > 0);
+  describe('getData({src:Bluelytics})', function(){
+    it('should return current dolar blue BUY and SELL rate, and a datetime', function(done){
+      dolarblue.getData({src:'Bluelytics'}, function(err, data) {
+        assert.ok(!err);
+        assert.ok( data.buy > 0);
+        assert.ok( data.sell > 0);
 
 
-                var d = data.datetime;
-                assert.ok( d > new Date(0) );
-                var before =  new Date('2013-12-16 18:00:00');
-                assert.ok( d > before);
-                done();
-            });
-        });
+        var d = data.datetime;
+        assert.ok( d > new Date(0) );
+        var before =  new Date('2013-12-16 18:00:00');
+        assert.ok( d > before);
+        done();
+      });
     });
+  });
 
 
-    describe('getData({src:ERROR})', function(){
-        it('should throw error', function(done){
-            dolarblue.getData({src:'ERROR'}, function(err, data) {
-                assert.ok(err === 'Unknown source provided: ERROR');
-                assert.ok(!data);
-                done();
-            });
-        });
+  describe('getData({src:ERROR})', function(){
+    it('should throw error', function(done){
+      dolarblue.getData({src:'ERROR'}, function(err, data) {
+        assert.ok(err === 'Unknown source provided: ERROR');
+        assert.ok(!data);
+        done();
+      });
     });
+  });
 
 
 });
